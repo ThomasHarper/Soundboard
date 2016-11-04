7f3b47c0a6242af6b188e6b03dc28da72ce00f6e
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-04 17:49:23 +0100

    Initial Commit

---
+++ Soundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soundboard
+//
+//  Created by Thomas Bentkowski on 04/11/2016.
+//  Copyright © 2016 Thomas Bentkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
