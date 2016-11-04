ade0195237d04a0f25112b0ea7e6333254fd740b
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-04 21:12:33 +0100

    Added NavigationViewController, creating SoundViewController and added outlets

---
+++ Soundboard/SoundViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Thomas Bentkowski on 04/11/2016.
+//  Copyright © 2016 Thomas Bentkowski. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    /////////////////// OUTLETS ////////////////////
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    ///////////////// PROPERTIES ///////////////////
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
