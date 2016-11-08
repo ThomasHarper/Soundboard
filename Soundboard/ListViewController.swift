6ac6e10e1a3351c4adbe84414cc08e85bff67806
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-08 15:53:59 +0100

    Setting up the tableView and audio player in order to play the audio when the user taps a cell

--- Soundboard/ListViewController.swift
+++ Soundboard/ListViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    /////////////////// OUTLETS ////////////////////
+    @IBOutlet weak var tableView: UITableView!
+    ///////////////// PROPERTIES ///////////////////
+    var sounds : [Sound]?
+    var audioPlayer : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            sounds = try context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        } catch {}
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let sound = sounds![indexPath.row]
+        
+        // Setting the name of the cell
+        cell.textLabel?.text = sound.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do {
+            // Instantiating audio player and playing the sound tapped
+            let sound = sounds![indexPath.row]
+            audioPlayer = try AVAudioPlayer(data: sound.audio! as Data)
+            audioPlayer?.play()
+        } catch {}
+        
+        // Deselecting the row tapped after the user tapped it
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
