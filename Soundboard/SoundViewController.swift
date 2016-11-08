c00cc66bd2bb9edc349e3c0b268954c53aaa8678
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-08 15:04:20 +0100

    Setting AVAudioRecorder and adding record action

--- Soundboard/SoundViewController.swift
+++ Soundboard/SoundViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
@@ -14,11 +15,62 @@ class SoundViewController: UIViewController {
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     ///////////////// PROPERTIES ///////////////////
+    var audioRecorder : AVAudioRecorder?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupRecorder()
+    }
+    
+    func setupRecorder() {
+        // Create an audio session that allows us to play and record audio
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker) // In order to play to speakers and not in ear speaker
+            try session.setActive(true)
+            
+        // Create URL for the audio file
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            let audioURL = NSURL.fileURL(withPathComponents: pathComponents)
+                
+        // Create settings for the audioRecorder
+            var settings : [String : AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+        // Create Audio Recorder Object
+            audioRecorder =  try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    @IBAction func recordTapped(_ sender: AnyObject) {
+        if audioRecorder!.isRecording {
+            // Stop the recording
+            audioRecorder?.stop()
+            // Change button title to "Record"
+            recordButton.setTitle("Record", for: .normal)
+        } else {
+            // Start the recording
+            audioRecorder?.record()
+            // Change button title to "Stop"
+            recordButton.setTitle("Stop", for: .normal)
+            
+        }
+    }
+    
+    @IBAction func playTapped(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+        
     }
 
     override func didReceiveMemoryWarning() {
